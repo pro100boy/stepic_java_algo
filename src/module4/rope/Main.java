c8404f66ae498d9683391f087d14657c57bd5012
Galushkin Pavel <gpg@ua.fm>
2017-05-17 17:28:40 +0300

    module 4, task 5. initial

--- src/module4/rope/Main.java
+++ src/module4/rope/Main.java
@@ -4,6 +4,57 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+class RopeString{
+    private boolean _isLeaf; // Является ли узел листом
+    private String _value; // Строка, хранимая в узле
+    private RopeString _leftChild; // Левый сын
+    private RopeString _rightChild; // Правый сын
+    private int _offset = 0; // Смещение относительно начала строки
+    private int _length; // Длина подстроки
+
+    public RopeString()
+    {
+        this("");
+    }
+
+    public RopeString(String value)
+    {
+        _value = value;
+        _isLeaf = true;
+        _length = value.length();
+    }
+
+    private RopeString(RopeString left, RopeString right)
+    {
+        _leftChild = left;
+        _rightChild = right;
+        _isLeaf = false;
+        _length = GetLength(left) + GetLength(right);
+    }
+    // Возвращает результат в узле
+    public String getValue() {
+        return _value;
+    }
+
+    // Возвращает длину результата
+    public int getLength() {
+        return _length;
+    }
+
+    // Вспомогательная функция для получения результата, избавляющая от проверки на null
+    private static String GetValue(RopeString rope)
+    {
+        return rope == null ? "" : rope.getValue();
+    }
+
+    // Вспомогательная функция для получения длины, избавляющая от проверки на null
+    private static int GetLength(RopeString rope)
+    {
+        return rope == null ? 0 : rope.getLength();
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
